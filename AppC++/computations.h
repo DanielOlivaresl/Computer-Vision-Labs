ea0504e6605119bd65eec9819c6b747e3de06352
Daniel <dolivaresl@outlook.com>
2024-03-08 14:29:37 -0600

    Fixed the issue where i could not link computations.cpp to computationsh.h and use the functions defined, by removing the static context of the functions

--- AppC++/computations.h
+++ AppC++/computations.h
@@ -2,13 +2,13 @@
 
 #include<vector>
 #include<Eigen/Dense>
-/*
-std::vector<double> static euclidean(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
-Eigen::MatrixXd static calculateCovMatrix(Eigen::MatrixXd data);
-std::vector<double> static manhalanobis(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
-std::vector<double> static max_prob(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
-int static getClosest(std::vector<double> distances);
+#include<iostream>
+#include<cmath>	
+
+std::vector<double> euclidean(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
+Eigen::MatrixXd  calculateCovMatrix(Eigen::MatrixXd data);
+std::vector<double> manhalanobis(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
+int getClosest(std::vector<double> distances);
 int getMaxProb(std::vector<double> probabilities);
+std::vector<double> max_prob(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
 
-
-*/
