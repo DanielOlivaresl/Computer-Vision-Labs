409f05666e9acc36b0ab4fc96667050799cdf5f7
Daniel <dolivaresl@outlook.com>
2024-03-07 19:58:42 -0600

    Added a lot of things

---
+++ AppC++/computations.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<vector>
+#include<Eigen/Dense>
+/*
+std::vector<double> static euclidean(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
+Eigen::MatrixXd static calculateCovMatrix(Eigen::MatrixXd data);
+std::vector<double> static manhalanobis(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
+std::vector<double> static max_prob(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, Eigen::Vector3d point);
+int static getClosest(std::vector<double> distances);
+int getMaxProb(std::vector<double> probabilities);
+
+
+*/
