7d8c374c0e4a653a13dd697e24e4d11732d6a0f3
Daniel <dolivaresl@outlook.com>
2024-04-01 19:06:45 -0600

    Added the cross validation functions

--- App QT/computations.cpp
+++ App QT/computations.cpp
@@ -299,3 +299,96 @@ int kNearestNeighbours(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> cla
 
 }
 
+
+
+// 50/50 split
+std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>>> CrossValidation::crossValidation(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes){
+		
+	//We will create a test and train split vector's of matrices 
+
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> testSplit;
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> trainingSplit;
+
+	//we will have train and test splits of the same size, and we will fill these randomly and have them be the same size
+
+	//to do this we will first iterate the original data
+
+	for (Eigen::Matrix<double, Eigen::Dynamic, 3> matrix : classes) {
+		Eigen::Matrix<double, Eigen::Dynamic, 3> test;
+		Eigen::Matrix<double, Eigen::Dynamic, 3> train;
+		
+
+		//we will randomly select indexes to add to the test set, until we have enough
+		std::vector<int> indexes;
+
+		while (indexes.size() < matrix.rows()/2) {
+			int index = std::rand() % matrix.rows();	
+			
+			//we check that the index isn't in the list 
+			while (!std::count(indexes.begin(), indexes.end(), index)) {
+				index = std::rand() % matrix.rows();
+			}
+
+			//Now that we know that the element isn't in the list we add it
+			indexes.push_back(index);
+		}
+
+		//We will now fill the test and training sets
+
+		for (int i = 0; i < matrix.rows(); i++) {
+			if (std::count(indexes.begin(), indexes.end(), i)) {
+				test.conservativeResize(test.rows() + 1, Eigen::NoChange);
+				test.row(test.rows() - 1) << matrix.row(i);
+			}
+			else {
+				train.conservativeResize(train.rows() + 1, Eigen::NoChange);
+				train.row(train.rows() - 1) << matrix.row(i);
+			}
+		}
+
+		testSplit.push_back(test);
+		trainingSplit.push_back(train);
+	}
+
+	//Final result
+
+	std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>>> result = { testSplit,trainingSplit };
+
+
+
+
+}
+
+// n-1/1 split
+//In the test set only one class will have data at one time thats the element we left out
+std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>>> CrossValidation::leaveOneOut(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes, int clas, int el) {
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> testSplit(classes.size());
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> trainingSplit = classes;
+
+	testSplit.at(clas) = (classes.at(clas).row(el));
+
+	Eigen::Matrix<double, Eigen::Dynamic, 3> tempTop = classes.at(clas).topRows(el);
+	Eigen::Matrix<double, Eigen::Dynamic, 3> tempBottom = classes.at(clas).bottomRows(classes.at(clas).rows()-el+1);
+
+	trainingSplit.at(clas).resize(classes.at(clas).rows() - 1, Eigen::NoChange);
+	
+	trainingSplit.at(clas) << tempTop, tempBottom;
+	
+	
+	std::vector<std::vector< Eigen::Matrix<double, Eigen::Dynamic, 3> > > result = { testSplit,trainingSplit };
+
+	return result;
+}
+// both splits of the same size
+std::vector<std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>>> CrossValidation::Restitucion(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> classes) {
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> testSplit = classes;
+	std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> trainingSplit = classes;
+
+	std::vector<std::vector< Eigen::Matrix<double, Eigen::Dynamic, 3> > > result = { testSplit,trainingSplit };
+
+	return result;
+
+
+	
+}
+
