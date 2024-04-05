7350e3488f0767220dd869bb485ed9ae06b43299
Carlos Ramírez <93393757+Thegr8carlos@users.noreply.github.com>
2024-04-05 11:40:14 -0600

    Added plotConfusionMatrix

--- App QT/plots.h
+++ App QT/plots.h
@@ -1,18 +1,21 @@
-#include <vector>
+// 
 #include <QScatterSeries>
 #include <QChartView>
 #include <QLineSeries>
 #include<QMainWindow>
-
- class Plots{
-
-
-
- public:
-
-	static void scatterPlot(std::vector<std::vector<double>> data);
+#include<Eigen/Dense>
+#include <QWidget>
+#include <QGridLayout>
+#include <QtCharts/QChart>
+#include <QtCharts/QChartView>
+class Plots {
 
 
+
+public:
+
+    static void scatterPlot(std::vector<std::vector<double>> data);
+    static void ConfusionMatrix(std::vector<std::vector<std::vector<double>>> matrices, std::vector<std::string> names);
 
 
 
