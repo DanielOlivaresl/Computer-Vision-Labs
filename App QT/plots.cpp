8735b04526cd2ded649de5ddddb3d75471d4d75a
Carlos Ramírez <93393757+Thegr8carlos@users.noreply.github.com>
2024-04-05 11:42:26 -0600

    Added matrixConfusion plot

--- App QT/plots.cpp
+++ App QT/plots.cpp
@@ -10,11 +10,11 @@ void Plots::scatterPlot(std::vector<std::vector<double>> data) {
     }
 
     // Create a chart
-    QtCharts::QChart* chart = new QtCharts::QChart();
+    QChart* chart = new QChart();
 
     // Create a single set of axes for the chart
-    QtCharts::QValueAxis* axisX = new QtCharts::QValueAxis(chart);
-    QtCharts::QValueAxis* axisY = new QtCharts::QValueAxis(chart);
+    QValueAxis* axisX = new QValueAxis(chart);
+    QValueAxis* axisY = new QValueAxis(chart);
     axisX->setTitleText("X Axis");
     axisY->setTitleText("Y Axis");
     axisX->setRange(-1, 5); // Set X axis range, adjust if needed
@@ -25,7 +25,7 @@ void Plots::scatterPlot(std::vector<std::vector<double>> data) {
     // Create and add scatter and line series to the chart
     for (size_t i = 0; i < data.size(); ++i) {
         // Create scatter series
-        QtCharts::QScatterSeries* scatter = new QtCharts::QScatterSeries(chart);
+        QScatterSeries* scatter = new QScatterSeries(chart);
         scatter->setName(QString::fromStdString(names[i]));
         for (size_t j = 0; j < data[i].size(); ++j) {
             scatter->append(j, data[i][j]);
@@ -35,7 +35,7 @@ void Plots::scatterPlot(std::vector<std::vector<double>> data) {
         scatter->attachAxis(axisY);
 
         // Create line series
-        QtCharts::QLineSeries* line = new QtCharts::QLineSeries(chart);
+        QLineSeries* line = new QLineSeries(chart);
         line->setName("Line " + QString::fromStdString(names[i])); // Unique name for each line series
         for (size_t j = 0; j < data[i].size(); ++j) {
             line->append(j, data[i][j]);
@@ -46,8 +46,114 @@ void Plots::scatterPlot(std::vector<std::vector<double>> data) {
     }
 
     // Create a chart view, set the chart, and display the chart view
-    QtCharts::QChartView* chartView = new QtCharts::QChartView(chart);
-    chartView->setRenderHint(QPainter::Antialiasing);
+    QChartView* chartView = new QChartView(chart);
+    chartView->setRenderHint(Antialiasing);
     chartView->resize(400, 300);
     chartView->show();
+}
+void Plots::ConfusionMatrix(std::vector<std::vector<std::vector<double>>> matrices, std::vector<std::string> names)
+{
+    int n = matrices.size();
+
+    QWidget* container = new QWidget(); // container of layouts
+    QGridLayout* layout = new QGridLayout(container);
+
+    int rows = std::sqrt(n);
+    int cols = std::ceil((double)n / rows);
+
+    const int sceneWidth = 500;
+    const int sceneHeight = 500;
+   
+    
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            int index = i * cols + j;
+            if (index < n) {
+
+                QGraphicsScene* scene = new QGraphicsScene(); // new instance of a graphicsScene (to draw the matrix)
+                scene->setSceneRect(0, 0, sceneWidth, sceneHeight);
+                
+                int matrixSize = std::min(matrices[index].size(), matrices[index][0].size()); 
+                double squareSize = std::min(sceneWidth / cols, sceneHeight / rows) / matrixSize; // size of each rect
+
+
+                // computes the sum of each row 
+                std::vector<double> rowSums(matrices[index].size(), 0.0);
+                for (int x = 0; x < matrices[index].size(); ++x) {
+                    for (int y = 0; y < matrices[index][x].size(); ++y) {
+                        qDebug() << " matrix" << x << "at value" << matrices[index][x][y];
+                        rowSums[x] += matrices[index][x][y];
+                    }
+                }
+
+                for (int x = 0; x < matrices[index].size(); ++x) {
+                    for (int y = 0; y < matrices[index][x].size(); ++y) {
+                        double squareX = x * squareSize;
+                        double squareY = y * squareSize;
+
+                        // computes the alpha value for each rect in the row
+                        float alpha;
+                        if (matrices[index][x][y] == 0) {
+                            alpha = 0;
+                        }
+                        else {
+                            alpha = matrices[index][x][y] / rowSums[x];
+                        }
+                        
+                        qDebug() << "Alpha value for matrix" << index << "at value" << matrices[index][x][y] << ":" << alpha;
+                        
+                        // color of the matrix
+                        QColor color(Qt::blue);
+                        color.setAlphaF(alpha);
+                        QBrush brush(color);
+
+                        // Drawing the rect and text
+                        QGraphicsRectItem* square = new QGraphicsRectItem(squareX, squareY, squareSize, squareSize);
+                        square->setBrush(brush);
+                        scene->addItem(square);
+                        QString text = QString("P = c - %1\nA = c - %2").arg(x).arg(y);
+                        QGraphicsTextItem* textItem = new QGraphicsTextItem(text);
+
+                        // Adjusting the size of the text
+                        QFont font = textItem->font();
+                        font.setPointSizeF(squareSize * 0.10); // a better way of adjusting the text??
+                        textItem->setFont(font);
+
+                        // Adjustiing the position 
+                        QFontMetrics fm(font);
+                        QRect rect = fm.boundingRect(text);
+                        qreal textWidth = rect.width()* .10;
+                        qreal textHeight = rect.height() * .10;
+                        textItem->setPos((squareX + (squareSize - textWidth) / 2)-10, squareY + (squareSize - textHeight) / 2);
+
+                        scene->addItem(textItem);
+                        scene->addItem(textItem);
+                    }
+                }
+
+                // Setting the view and the alignment
+                QGraphicsView* graphicsView = new QGraphicsView(scene);
+                graphicsView->setRenderHint(QPainter::Antialiasing);
+                graphicsView->setAlignment(Qt::AlignTop | Qt::AlignLeft | Qt::AlignRight); // Alinear la vista arriba y a la izquierda
+                
+                // settin a box layout
+                QVBoxLayout* vbox = new QVBoxLayout();
+                vbox->addWidget(graphicsView);
+
+                // setting the title of each confusion matrix
+                if (index < names.size()) {
+                    QLabel* titleLabel = new QLabel(QString::fromStdString(names[index]));
+                    titleLabel->setAlignment(Qt::AlignCenter);
+                    vbox->addWidget(titleLabel);
+                }
+
+                // Adding the layout to the container
+                layout->addLayout(vbox, i, j);
+            }
+        }
+    }
+
+    container->setLayout(layout);
+    container->resize(800, 600);
+    container->show();
 }
