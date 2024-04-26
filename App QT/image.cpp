741e04342af7a1ee029c1377456e19a78e7c6eeb
Carlos Ramírez <93393757+Thegr8carlos@users.noreply.github.com>
2024-04-26 00:23:40 -0600

    Added image.cpp and image.h

---
+++ App QT/image.cpp
@@ -0,0 +1,21 @@
+#include "image.h"	
+#include<QDebug>
+
+
+Image::Image(QObject* parent) : QObject(parent) {
+    qDebug() << "Image default constructor called";
+}
+
+Image::~Image() {
+    qDebug() << "Image default destructor called";
+
+}
+
+Image::Image(const Image& other) : QObject(other.parent()) {
+    this->numClasses = other.numClasses;
+    this->currProcess = other.currProcess;
+    this->rectangles = other.rectangles;
+    this->matrixClasses = other.matrixClasses;
+    this->image = other.image;
+
+}
