0d10194ebdd39580cd5119101e186c0d8db3b1d9
Carlos Ramírez <93393757+Thegr8carlos@users.noreply.github.com>
2024-05-05 20:38:28 -0600

    added matrixPlot2D and plotPerceptrons firms

--- App QT/plots.h
+++ App QT/plots.h
@@ -11,17 +11,22 @@
 #include <QDebug>
 #include <QtCharts/QBarSeries>
 #include <QtCharts/QBarSet>
-
+#include <QtDataVisualization/Q3DScatter>
+#include <QtDataVisualization/QScatterDataProxy>
+#include <QtDataVisualization/QScatter3DSeries>
+#include <QWidget>
+#include <QColor>
 #include "computations.h"
+#include "machineLearning.h"
 class Plots {
 
 
 
 public:
-
+    static void matrixPlot2D(Eigen::MatrixXd values,std::string col1Name, std::string col2Name);
     static void scatterPlot(std::vector<std::vector<double>> data);
     static void ConfusionMatrix(std::vector<std::vector<std::vector<double>>> matrices, std::vector<std::string> names);
     static void histogram(std::vector<int> histogram);
-
+    static void plotPerceptrons(std::vector<Perceptron> perceptrons);
 
 };
