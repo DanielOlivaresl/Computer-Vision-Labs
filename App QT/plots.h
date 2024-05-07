4633e2246ce5073c563a10f52cca0a390e188bfd
Carlos Ramírez <93393757+Thegr8carlos@users.noreply.github.com>
2024-05-06 21:43:23 -0600

    Added matrix plotting firms

--- App QT/plots.h
+++ App QT/plots.h
@@ -23,7 +23,10 @@ class Plots {
 
 
 public:
-    static void matrixPlot2D(Eigen::MatrixXd values,std::string col1Name, std::string col2Name);
+    static void matrixPlot3D_labels(Eigen::MatrixXd values,std::vector<std::string> labels, std::string col1Name, std::string col2Name, std::string col3Name); // plots a 3d matrix data where each entry in the labels vector is the label of each row
+    static void plotMatrix(Eigen::MatrixXd matrixData, std::vector<std::string> colNames); // used to handle the plotMatrix diferent procedures
+    static void matrixPlot2D(Eigen::MatrixXd values,std::string col1Name, std::string col2Name); // simple plotting 
+    static void matrixPlot3D(Eigen::MatrixXd values, std::string col1Name, std::string col2Name, std::string col3Name);
     static void scatterPlot(std::vector<std::vector<double>> data);
     static void ConfusionMatrix(std::vector<std::vector<std::vector<double>>> matrices, std::vector<std::string> names);
     static void histogram(std::vector<int> histogram);
