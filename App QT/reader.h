54133549f717334409e14fa9edf0478c80d6f91b
Carlos Ramírez <93393757+Thegr8carlos@users.noreply.github.com>
2024-05-06 21:44:30 -0600

    Update reader.h with structure DataStore and read csv to data store

--- App QT/reader.h
+++ App QT/reader.h
@@ -5,6 +5,12 @@
 #include<qstring.h>
 #include <QDebug>
 #include <cctype>
+struct DataStore
+{
+	Eigen::MatrixXd numericData;
+	std::vector<std::vector<std::string>> stringData;
+};
 Eigen::MatrixXd read_csv(QString filePath); // this reader does not store string variables
+DataStore read_csvToDataStore(QString filePath);
 
 
