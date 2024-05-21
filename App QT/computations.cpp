7557c2998cbce821c1ba85fda3a0674afcec5247
Carlos Ramírez <93393757+Thegr8carlos@users.noreply.github.com>
2024-05-21 07:28:15 -0600

    Update computations.cpp

--- App QT/computations.cpp
+++ App QT/computations.cpp
@@ -527,11 +527,11 @@ void normalizeColumn(Eigen::MatrixXd& matrix, int column) {
 }
 int clockwise(int dir)
 {
-	return (dir%8) + 1;
+	return (dir % 8) + 1;
 }
 int counterclockwise(int dir)
 {
-	return (dir+6)%8 + 1;
+	return (dir + 6) % 8 + 1;
 }
 std::vector<std::vector<double>> get_matrixConfusion(std::vector<Eigen::Matrix<double, Eigen::Dynamic, 3>> mat, std::vector<std::vector<int>> vectorOfPredictions) {
 
@@ -547,7 +547,7 @@ std::vector<std::vector<double>> get_matrixConfusion(std::vector<Eigen::Matrix<d
 
 std::vector<int> ObjectMetrics::calculateArea(QVector<QPoint> points, QImage& image)
 {
-	
+
 	std::sort(points.begin(), points.end(), [](const QPoint& a, const QPoint& b) {
 		return a.x() < b.x();
 		});
@@ -582,7 +582,7 @@ std::vector<int> ObjectMetrics::calculateArea(QVector<QPoint> points, QImage& im
 std::vector<int> ObjectMetrics::calculatePerimeter(QVector<QPoint> object, QImage& image)
 {
 	int res = object.size();
-	
+
 	std::vector<int> resultingMet;
 	resultingMet.push_back(res);
 	return resultingMet;
@@ -613,16 +613,40 @@ std::vector<int> ObjectMetrics::calculateCenterOfGravity(QVector<QPoint> object,
 
 Eigen::MatrixXd ObjectMetrics::featureExtraction(std::vector<std::function<std::vector<int>(QVector<QPoint>, QImage&)>> functions, QVector<QPoint> object, QImage& image)
 {
-	Eigen::MatrixXd descriptors(1, functions.size());
+	int minX = object[0].x();
+	int minY = object[0].y();
+	int maxX = object[0].x();
+	int maxY = object[0].y();
+	// Find the bounding box of the object
+	for (const QPoint& point : object) {
+		if (point.x() < minX) {
+			minX = point.x();
+		}
+		if (point.x() > maxX) {
+			maxX = point.x();
+		}
+		if (point.y() < minY) {
+			minY = point.y();
+		}
+		if (point.y() > maxY) {
+			maxY = point.y();
+		}
+	}
+
+	// Create a new image containing only the object
+	QImage objectImage = image.copy(minX - 5, minY - 5, maxX - minX + 10, maxY - minY + 10);
+	//QImage objectImageBinary = thereshold(objectImage, 130);
+	double e = ObjectMetrics::calculateEccentricity((objectImage));
+	Eigen::MatrixXd descriptors(1, functions.size() + 1 + 1);
 	int metricCount = 0;
-	for (auto func: functions) {
-		
+	for (auto func : functions) {
+
 
 
 		std::vector<int> currMetric = func(object, image);
 
 		//we iterate the current metrics values
-		
+
 		if (currMetric.size() > 1) {
 			descriptors.conservativeResize(descriptors.rows(), descriptors.cols() + currMetric.size() - 1);
 		}
@@ -631,20 +655,59 @@ Eigen::MatrixXd ObjectMetrics::featureExtraction(std::vector<std::function<std::
 			descriptors(0, metricCount) = metric;
 			metricCount++;
 		}
-
-
-	}
-	
-	
-	
-	
-	
-	
-	
+	}// area, per
+	descriptors(0, metricCount) = e;
+	double c = imageMoments(objectImage,0,0);
+	descriptors(0, metricCount + 1) = c;
 	return descriptors;
 }
 
+double ObjectMetrics::imageMoments(QImage& binarizedImage, int p, int q)
+{
+	double res = 0;
+	for (int y = 0; y < binarizedImage.height(); y++) {
+		for (int x = 0; x < binarizedImage.width(); x++) {
+			int pixelVal = qGray(binarizedImage.pixel(x, y));
+			if (pixelVal == 255) { // Asume que la imagen está binarizada con fondo negro (0) y objetos blancos (255)
+				res += pow(x, q) * pow(y, p);
+			}
+		}
+	}
+	return res;
+}
 
+double ObjectMetrics::centralMoments(QImage& binarizedImage, int p, int q, double xCent, double yCent)
+{
+	double res = 0;
+	for (int y = 0; y < binarizedImage.height(); y++) {
+		for (int x = 0; x < binarizedImage.width(); x++) {
+			int pixelVal = qGray(binarizedImage.pixel(x, y));
+			if (pixelVal == 255) { // Asume que la imagen está binarizada con fondo negro (0) y objetos blancos (255)
+				res += pow(x - xCent, p) * pow(y - yCent, q);
+			}
+		}
+	}
+	return res;
+}
 
+double ObjectMetrics::calculateEccentricity(QImage& image)
+{
+	double m00 = imageMoments(image, 0, 0);
+	double m10 = imageMoments(image, 1, 0);
+	double m01 = imageMoments(image, 0, 1);
 
+	double cx = m10 / m00;
+	double cy = m01 / m00;
 
+	double mu20 = centralMoments(image, 2, 0, cx, cy);
+	double mu02 = centralMoments(image, 0, 2, cx, cy);
+	double mu11 = centralMoments(image, 1, 1, cx, cy);
+
+	double a = sqrt(2 * (mu20 + mu02 + sqrt(pow(mu20 - mu02, 2) + 4 * pow(mu11, 2))));
+	double b = sqrt(2 * (mu20 + mu02 - sqrt(pow(mu20 - mu02, 2) + 4 * pow(mu11, 2))));
+
+	double c = sqrt(a * a - b * b);
+	double e = c / a;
+
+	return 200.000*e;
+}
